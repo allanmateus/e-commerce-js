7dacd73fdd0fb22bcb4a23975d031023157e1c67
Allan Mateus <72580107+allanmateus@users.noreply.github.com>
2023-08-14 17:49:15 -0300

    Add files via upload

--- pedido.js
+++ pedido.js
@@ -1,24 +1,44 @@
+
+// Criando a classe de Pedidos
 class Pedido {
+    // Criando um atributo estático que auto incrementa a cada instância criada.
     static pedidosCont = 0;
     constructor(valorPedido, statusPedido){
-        this.id = ++this.constructor.pedidosCont;
+        this.id = ++Pedido.pedidosCont;
         this.valor = valorPedido;
         this.status = statusPedido;
     }
 }
 
+
 listaDePedidos = [];
 
+// Criando as arrow functions 
+
 adicionarPedido = ((valorPedido, statusPedido) => {
-    const novoPedido = new Pedido(valorPedido, statusPedido);
+    let novoPedido = new Pedido(valorPedido, statusPedido);
     listaDePedidos.push(novoPedido);
 });
 
 listarPedidosPorStatus = ((statusPedido) => {
     let pedidoPorStatus = listaDePedidos.filter((pedido) => pedido.status === statusPedido);
-    for (pedidoFiltrado in pedidoPorStatus){
-        console.log(`Pedido # ${pedidoFiltrado.index} - Valor: R$ ${pedidoFiltrado.VALOR}`);
+    for ( pedidoFiltrado of pedidoPorStatus ){
+        console.log(`Pedido # ${pedidoFiltrado.id} - Valor: R$ ${pedidoFiltrado.valor.toFixed(2)}`);
     }
+});
+
+calcularTotalDePedidos = (() =>{
+    let totalPedidos = listaDePedidos.reduce((soma, object) => soma + object.valor,0);
+
+    console.log(`Total de pedidos: R$ ${totalPedidos.toFixed(2)}`);
+});
+
+atualizarStatus = ((statusAtualizado) => {
+    for (let i = 0; i < listaDePedidos.length; i++){
+        if (listaDePedidos[i] === pedidoParaAtualizar){
+            listaDePedidos[i].status = statusAtualizado
+        };
+    };
 });
 
 adicionarPedido(100, "Aguardando Pagamento");
@@ -26,9 +46,11 @@ adicionarPedido(150, "Enviado");
 adicionarPedido(80, "Entregue");
 adicionarPedido(200, "Aguardando Pagamento");
 
-
 listarPedidosPorStatus("Aguardando Pagamento");
 
+const pedidoParaAtualizar = listaDePedidos[0];
+pedidoParaAtualizar.atualizarStatus("Pago");
 
+calcularTotalDePedidos();
 
 
